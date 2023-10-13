bd46854dd45fe7fb391283584d642a8f561f1a75
Student <student@Rental-Mac-50.app.byu.edu>
2023-10-13 15:47:07 -0600

    batch

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -3,18 +3,18 @@ library(embed)
 library(vroom)
 
 # Read in the data
-setwd("/Users/student/Desktop/STAT348")
-amazon_training  <- vroom("AmazonEmployeeAccess/train.csv")
-amazon_test <- vroom("AmazonEmployeeAccess/test.csv")
+#setwd("/Users/student/Desktop/STAT348/AmazonEmployeeAccess")
+amazon_training  <- vroom("train.csv")
+amazon_test <- vroom("test.csv")
 amazon_training$ACTION <- as.factor(amazon_training$ACTION)
 
 
 # at least 2 exploratory plots for the amazon data
-ggplot(amazon, aes(x = ACTION)) +
-  geom_bar()
-
-ggplot(amazon, aes(x = ROLE_TITLE)) +
-  geom_bar()
+# ggplot(amazon, aes(x = ACTION)) +
+#   geom_bar()
+# 
+# ggplot(amazon, aes(x = ROLE_TITLE)) +
+#   geom_bar()
 
 sorted_counts <- sort(table(amazon_training$ROLE_TITLE), decreasing = T)
 sorted_counts_20 <- head(sorted_counts, 20)
@@ -29,14 +29,34 @@ ggplot(df_counts_20, aes(x = Count.Var1, y = Count.Freq)) +
 my_recipe <- recipe(ACTION~., data=amazon_training) %>%
 step_mutate_at(all_numeric_predictors(), fn = factor) %>% # turn all numeric features into factors
   step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <5% into an "other" value
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(target_var))
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
 prep <- prep(my_recipe)
 bake(prep, new_data=amazon_training)
 bake(prep, new_data=amazon_test)
 
-setwd("/Users/student/Desktop/STAT348/AmazonEmployeeAccess")
+#setwd("/Users/student/Desktop/STAT348/AmazonEmployeeAccess")
 
+
+# Logistic Regression -----------------------------------------------------
+library(tidymodels)
+
+my_mod <- logistic_reg() %>% #Type of model
+  set_engine("glm")
+
+amazon_workflow <- workflow() %>%
+add_recipe(my_recipe) %>%
+add_model(my_mod) %>%
+fit(data = amazon_training) # Fit the workflow
+
+amazon_predictions <- predict(amazon_workflow,
+                              new_data=amazon_test,
+                              type="prob") %>%
+                      bind_cols(amazon_test) %>%
+                      rename(ACTION=.pred_1) %>%
+                      select(id, ACTION)
+vroom_write(x = amazon_predictions, file = "amazonlogisticregression2.csv", delim = ",")
+getwd()
 
 # Penalized Logistic Regression -------------------------------------------
 
@@ -70,12 +90,18 @@ select_best("roc_auc")
 
 ## Finalize the Workflow & fit it
 final_wf <-
-preg_wf %>%
+  amazon_workflow %>%
 finalize_workflow(bestTune) %>%
-fit(data=myDataSet)
+fit(data=amazon_training)
 
 ## Predict
-final_wf %>%
-predict(new_data = myNewData, type=)
+amazon_predictions <- final_wf %>%
+  predict(new_data = amazon_test, type="prob") %>%
+  bind_cols(amazon_test) %>%
+  rename(ACTION=.pred_1) %>%
+  select(id, ACTION)
+vroom_write(x = amazon_predictions, file = "penalized", delim = ",")
 
-#1. penalty, #2. mixture
+# save(file = "filename.RData" , list = c("LogRge_wf"))
+# load("filename.RData")
+
